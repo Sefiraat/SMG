c3697160ff87ebfedf0846fe17ea446eec8bebbd
jayman820 <josh@jgottschall.com>
2022-08-05 11:08:49 -0400

    Added deepslate redstone SMG recipe Added lapis lazuli SMGs Added coal SMGs Added an amethyst SMG Adjusted the recipe for the redstone SMG to work with the lapis lazuli one Upated the spigot dependency to 1.18.2 in pom.xml

--- src/main/java/me/waleks/simplematerialgenerators/SMGItemSetup.java
+++ src/main/java/me/waleks/simplematerialgenerators/SMGItemSetup.java
@@ -176,7 +176,7 @@ public final class SMGItemSetup {
                 SMGItems.SMG_GENERATOR_REDSTONE_BADLY_FORMED,
                 RecipeType.ENHANCED_CRAFTING_TABLE,
                 new ItemStack[] {
-                    SMGItems.SMG_GENERATOR_SAND, null, SMGItems.SMG_GENERATOR_GRAVEL,
+                    SMGItems.SMG_GENERATOR_SAND, new ItemStack(Material.REDSTONE), SMGItems.SMG_GENERATOR_GRAVEL,
                     null, null, null,
                     null, null, null
                 })
@@ -204,6 +204,122 @@ public final class SMGItemSetup {
             }, 24)
             .setItem(Material.REDSTONE)
             .register(plugin);
+
+        new MaterialGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_DEEPSLATE_REDSTONE,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[] {
+                        SMGItems.SMG_GENERATOR_REDSTONE_BROKEN, new ItemStack(Material.DEEPSLATE_REDSTONE_ORE), SMGItems.SMG_GENERATOR_REDSTONE_BROKEN,
+                        SMGItems.SMG_GENERATOR_REDSTONE_BROKEN, SMGItems.SMG_GENERATOR_GRAVEL, SMGItems.SMG_GENERATOR_REDSTONE_BROKEN,
+                        SlimefunItems.DAMASCUS_STEEL_INGOT, SlimefunItems.DURALUMIN_INGOT, SlimefunItems.DAMASCUS_STEEL_INGOT
+                }, 24)
+                .setItem(Material.REDSTONE)
+                .register(plugin);
+
+        new BrokenGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_LAPIS_BADLY_FORMED,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[] {
+                        SMGItems.SMG_GENERATOR_SAND, new ItemStack(Material.LAPIS_LAZULI), SMGItems.SMG_GENERATOR_GRAVEL,
+                        null, null, null,
+                        null, null, null
+                })
+                .register(plugin);
+
+        new BrokenGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_LAPIS_BROKEN,
+                RecipeType.ORE_CRUSHER,
+                new ItemStack[] {
+                        SMGItems.SMG_GENERATOR_LAPIS_BADLY_FORMED, null, null,
+                        null, null, null,
+                        null, null, null
+                })
+                .register(plugin);
+
+        new MaterialGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_LAPIS,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[] {
+                        SMGItems.SMG_GENERATOR_LAPIS_BROKEN, new ItemStack(Material.LAPIS_ORE), SMGItems.SMG_GENERATOR_LAPIS_BROKEN,
+                        SMGItems.SMG_GENERATOR_LAPIS_BROKEN, SMGItems.SMG_GENERATOR_GRAVEL, SMGItems.SMG_GENERATOR_LAPIS_BROKEN,
+                        SlimefunItems.DAMASCUS_STEEL_INGOT, SlimefunItems.DURALUMIN_INGOT, SlimefunItems.DAMASCUS_STEEL_INGOT
+                }, 24)
+                .setItem(Material.LAPIS_LAZULI)
+                .register(plugin);
+
+        new MaterialGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_DEEPSLATE_LAPIS,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[] {
+                        SMGItems.SMG_GENERATOR_LAPIS_BROKEN, new ItemStack(Material.DEEPSLATE_LAPIS_ORE), SMGItems.SMG_GENERATOR_LAPIS_BROKEN,
+                        SMGItems.SMG_GENERATOR_LAPIS_BROKEN, SMGItems.SMG_GENERATOR_GRAVEL, SMGItems.SMG_GENERATOR_LAPIS_BROKEN,
+                        SlimefunItems.DAMASCUS_STEEL_INGOT, SlimefunItems.DURALUMIN_INGOT, SlimefunItems.DAMASCUS_STEEL_INGOT
+                }, 24)
+                .setItem(Material.LAPIS_LAZULI)
+                .register(plugin);
+
+        new BrokenGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_COAL_BADLY_FORMED,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                    SMGItems.SMG_GENERATOR_STONE, null, SMGItems.SMG_GENERATOR_NETHERRACK,
+                        null, null, null,
+                        null, null, null
+                })
+                .register(plugin);
+
+        new BrokenGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_COAL_BROKEN,
+                RecipeType.ORE_CRUSHER,
+                new ItemStack[]{
+                        SMGItems.SMG_GENERATOR_COAL_BADLY_FORMED, null, null,
+                        null, null, null,
+                        null, null, null
+                })
+                .register(plugin);
+
+        new MaterialGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_COAL,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        SMGItems.SMG_GENERATOR_COAL_BROKEN, new ItemStack(Material.COAL_ORE), SMGItems.SMG_GENERATOR_COAL_BROKEN,
+                        SMGItems.SMG_GENERATOR_COAL_BROKEN, SMGItems.SMG_GENERATOR_STONE, SMGItems.SMG_GENERATOR_COAL_BROKEN,
+                        SlimefunItems.COMPRESSED_CARBON, SlimefunItems.DAMASCUS_STEEL_INGOT, SlimefunItems.COMPRESSED_CARBON
+                }, 24)
+                .setItem(Material.COAL)
+                .register(plugin);
+
+        new MaterialGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_DEEPSLATE_COAL,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        SMGItems.SMG_GENERATOR_COAL_BROKEN, new ItemStack(Material.DEEPSLATE_COAL_ORE), SMGItems.SMG_GENERATOR_COAL_BROKEN,
+                        SMGItems.SMG_GENERATOR_COAL_BROKEN, SMGItems.SMG_GENERATOR_STONE, SMGItems.SMG_GENERATOR_COAL_BROKEN,
+                        SlimefunItems.COMPRESSED_CARBON, SlimefunItems.DAMASCUS_STEEL_INGOT, SlimefunItems.COMPRESSED_CARBON
+                }, 24)
+                .setItem(Material.COAL)
+                .register(plugin);
+
+        new MaterialGenerator(
+                SMGItems.SMG_ITEM_CATEGORY,
+                SMGItems.SMG_GENERATOR_AMETHYST,
+                RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{
+                        SMGItems.SMG_GENERATOR_OBSIDIAN, SMGItems.SMG_GENERATOR_STONE, SMGItems.SMG_GENERATOR_GLASS,
+                        null, null, null,
+                        null, null, null
+                }, 18)
+                .setItem(Material.AMETHYST_BLOCK)
+                .register(plugin);
 
         new BrokenGenerator(
             SMGItems.SMG_ITEM_CATEGORY,
